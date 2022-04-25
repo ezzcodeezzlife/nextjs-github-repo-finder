7ca119c53275643b821c82ec72430fcbb4fba72d
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-04-25 17:50:34 +0200

    add homepage

--- pages/[language].js
+++ pages/[language].js
@@ -28,18 +28,35 @@ const Language = () => {
   const [loading, setLoading] = useState(true);
 
   const fetchData = () => {
-    fetch(
-      "https://api.github.com/search/repositories?q=" +
-        String(language) +
-        "&sort=stars&order=desc&per_page=4" /// + topic
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((repos) => {
-        setRepos(repos.items);
-        setLoading(false);
-      });
+    if (!topic) {
+      fetch(
+        "https://api.github.com/search/repositories?q=" +
+          String(language) +
+          "&sort=stars&order=desc&per_page=4" /// + topic
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((repos) => {
+          setRepos(repos.items);
+          setLoading(false);
+        });
+    } else {
+      fetch(
+        "https://api.github.com/search/repositories?q=" +
+          String(language) +
+          " " +
+          String(topic) +
+          "&sort=stars&order=desc&per_page=8" /// + topic
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((repos) => {
+          setRepos(repos.items);
+          setLoading(false);
+        });
+    }
   };
 
   useEffect(() => {
@@ -51,86 +68,77 @@ const Language = () => {
 
   return (
     <>
-      
-        <nav
-          class="navbar navbar-dark bg-dark"
-          aria-label="First navbar example"
-        >
-          <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-              Repofy
-            </a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarsExample01"
-              aria-controls="navbarsExample01"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
+      <nav class="navbar navbar-dark bg-dark" aria-label="First navbar example">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            Repofy
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarsExample01"
+            aria-controls="navbarsExample01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-            <div class="collapse navbar-collapse" id="navbarsExample01">
-              <ul class="navbar-nav me-auto mb-2">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Link
-                  </a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link dropdown-toggle"
-                    href="#"
-                    id="dropdown01"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    All anguages
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="dropdown01">
-                    <li>
-                      <a class="dropdown-item" href="/python">
-                        Python
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/java">
-                        Java
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/go">
-                        Go
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
+          <div class="collapse navbar-collapse" id="navbarsExample01">
+            <ul class="navbar-nav me-auto mb-2">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="dropdown01"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  All anguages
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdown01">
+                  <li>
+                    <a class="dropdown-item" href="/python">
+                      Python
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/java">
+                      Java
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/go">
+                      Go
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </div>
-        </nav>
+        </div>
+      </nav>
 
-
-      <div class="container" style={{paddingTop: "2vh"}}>
+      <div class="container" style={{ paddingTop: "2vh" }}>
         {name ? (
           <div class="jumbotron">
             <h1 class="display-4">Hello, {name}!</h1>
             <p class="lead">
-              We found your repository <b>{reponame} </b> and it looks really interesting.
+              We found your repository <b>{reponame} </b> and it looks really
+              interesting.
             </p>
-            <p class="lead">Because you like the topic <b>{topic}</b> you should have a look at this:</p>
+            <p class="lead">
+              Because you like the topic <b>{topic}</b> you should have a look
+              at this:
+            </p>
             <hr class="my-4"></hr>
-            
-
-            
-
 
             <p class="lead">
               <a class="btn btn-primary btn-lg" href="#content" role="button">
@@ -139,178 +147,201 @@ const Language = () => {
             </p>
           </div>
         ) : (
+          <h2> {language} </h2>
+        )}
+
+        <center>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            repos.map((repo) => (
+              <div class="card" id="content" style={{ marginTop: "1vw" }}>
+                <Card>
+                  <Card.Body>
+                    <Card.Title> {repo.name}</Card.Title>
+                    <Card.Text>{repo.description}</Card.Text>
+                    <Button
+                      id="homepagebutton"
+                      href={repo.html_url}
+                      variant="primary"
+                    >
+                      <GoMarkGithub></GoMarkGithub> GitHub
+                    </Button>{" "}
+                    <Button
+                      id="homepagebutton"
+                      href={repo.homepage}
+                      variant="secondary"
+                    >
+                      <GoGlobe></GoGlobe> Homepage
+                    </Button>
+                  </Card.Body>
+                  <Card.Header>
+                    <Badge bg="secondary"> ⭐ {repo.stargazers_count}</Badge> ▪️{" "}
+                    <Badge bg="info">{repo.language}</Badge>
+                  </Card.Header>
+                </Card>
+              </div>
+            ))
+          )}
+        </center>
+
+        {name ? (
+          <center>
+            <div style={{ paddingTop: "2vh" }}>
+              <form action={"/" + language}>
+                <button className="btn btn-outline-primary" type="submit">
+                  See more {language} projects
+                </button>
+              </form>
+            </div>
+          </center>
+        ) : (
           <></>
         )}
 
-      
-
-
-
-
-<center>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          repos.map((repo) => (
-            
-            <div class="card"  id="content" style={{marginTop: "1vw"}}>
-            <Card>
-              <Card.Body>
-                <Card.Title> {repo.name}</Card.Title>
-                <Card.Text>{repo.description}</Card.Text>
-                <Button
-                  id="homepagebutton"
-                  href={repo.html_url}
-                  variant="primary"
-                >
-                  <GoMarkGithub></GoMarkGithub> GitHub
-                </Button>{" "}
-                <Button
-                  id="homepagebutton"
-                  href={repo.homepage}
-                  variant="secondary"
-                >
-                  <GoGlobe></GoGlobe> Homepage
-                </Button>
-              </Card.Body>
-              <Card.Header>
-                <Badge bg="secondary"> ⭐ {repo.stargazers_count}</Badge> ▪️{" "}
-                <Badge bg="info">{repo.language}</Badge>
-              </Card.Header>
-            </Card>
-          </div>
-
-          
-          ))
-        )}
-</center>
-
-
-        <center>
-        <div style={{paddingTop: "2vh"}}>
-        <form action={"/" + language}>
-          <button className="btn btn-outline-primary" type="submit">
-            See more {language} projects
-          </button>
-        </form>
-        </div>
-        </center> 
-       
-       
-       
-        <div class="card text-center" style={{marginTop: "2vh"}}>
-          <div class="card-header">
-            Featured
-          </div>
+        <div class="card text-center" style={{ marginTop: "2vh" }}>
+          <div class="card-header">Featured</div>
           <a href={"https://de.coursera.org/search?query=" + language}>
-          <div class="card-body">
-            <h5 class="card-title">Check out these courses for: {language}{" "}</h5>
-            <p class="card-text">Many of them cover the topic "{topic}"!</p>
-            <a class="btn btn-primary">Go somewhere</a>
-          </div>
+            <div class="card-body">
+              <h5 class="card-title">
+                Check out these courses for: {language}{" "}
+              </h5>
+              {topic ? (
+                <p class="card-text">Many of them cover the topic "{topic}"!</p>
+              ) : (
+                <p class="card-text">
+                  Many of them cover the topics you would like!
+                </p>
+              )}
+
+              <a class="btn btn-primary">Go somewhere</a>
+            </div>
           </a>
-          <div class="card-footer text-muted">
-            2 days ago
+          <div class="card-footer text-muted">2 days ago</div>
+        </div>
+      </div>
+
+      <footer
+        class="bg-dark text-center text-lg-start"
+        style={{ marginTop: "2vh", color: "white" }}
+      >
+        <div class="container p-4">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase">All languages</h5>
+
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="/Python" style={{ color: "white" }}>
+                    Python
+                  </a>
+                </li>
+                <li>
+                  <a href="/Go" style={{ color: "white" }}>
+                    Go
+                  </a>
+                </li>
+                <li>
+                  <a href="/C++" style={{ color: "white" }}>
+                    C++
+                  </a>
+                </li>
+                <li>
+                  <a href="/Javascript" style={{ color: "white" }}>
+                    Javascript
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase mb-0">Links</h5>
+
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Links</h5>
+
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase mb-0">Links</h5>
+
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" style={{ color: "white" }}>
+                    Link 1
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
+
+        <div class="text-center p-3">
+          © 2020 Copyright:
+          <a href="https://mdbootstrap.com/">Repofy.com</a>
         </div>
-
-        <footer class="bg-dark text-center text-lg-start" style={{marginTop: "2vh", color:"white"}}>
-
-  <div class="container p-4">
-   
-    <div class="row">
-     
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">All languages</h5>
-
-        <ul class="list-unstyled mb-0">
-          <li>
-            <a href="/Python" style={{color:"white"}}>Python</a>
-          </li>
-          <li>
-          <a href="/Go" style={{color:"white"}}>Go</a>
-          </li>
-          <li>
-          <a href="/C++" style={{color:"white"}}>C++</a>
-          </li>
-          <li>
-          <a href="/Javascript" style={{color:"white"}}>Javascript</a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-0">Links</h5>
-
-        <ul class="list-unstyled mb-0">
-          <li>
-            <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-          <li>
-          <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-          <li>
-          <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-          <li>
-          <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Links</h5>
-
-        <ul class="list-unstyled mb-0">
-          <li>
-            <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-          <li>
-          <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-          <li>
-          <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-          <li>
-          <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-0">Links</h5>
-
-        <ul class="list-unstyled mb-0">
-          <li>
-            <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-          <li>
-          <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-          <li>
-          <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-          <li>
-          <a href="#!" style={{color:"white"}}>Link 1</a>
-          </li>
-        </ul>
-      </div>
-  
-    </div>
-
-  </div>
-
-
-
-  <div class="text-center p-3">
-    © 2020 Copyright:
-    <a href="https://mdbootstrap.com/">Repofy.com</a>
-  </div>
- 
-</footer>
-
-    
+      </footer>
     </>
   );
 };
