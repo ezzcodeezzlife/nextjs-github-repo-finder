02068b558744c977aaeced9a1abc3d0f6be0836e
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-04-26 00:15:38 +0200

    +

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css"; // Add this line
 import { useState, useEffect } from "react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Script id="Adsense-id" data-ad-client="ca-pub-8251732556629149"
+  async strategy="afterInteractive"
+  onError={ (e) => { console.error('Script failed to load', e) }}
+src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+/>
+    
+  <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
